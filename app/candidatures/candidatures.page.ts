c55468b02933e3f09b934a648e6cd58035b6d2c5
T-bueno <81462358+T-bueno@users.noreply.github.com>
2021-05-14 11:30:14 +0200

    candidatures.pages

---
+++ app/candidatures/candidatures.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidatures',
+  templateUrl: './candidatures.page.html',
+  styleUrls: ['./candidatures.page.scss'],
+})
+export class CandidaturesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
